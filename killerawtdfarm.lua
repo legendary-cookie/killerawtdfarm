a2acff5b369ec39bfc5f99dee663163f9b5fef70
legendary cookie <66409218+legendary-cookie@users.noreply.github.com>
2024-10-28 19:43:13 +0800

    Update killerawtdfarm.lua

--- killerawtdfarm.lua
+++ killerawtdfarm.lua
@@ -1,27 +1,59 @@
 repeat wait() until game:IsLoaded()
+wait(2)
 
+--auto tp, replay and play
 getgenv().AutoPlaceUnit = true
-getgenv().AutoSkip = false
 getgenv().AutoFirstSkip = true
 getgenv().Auto3xSpeed = true
 getgenv().AutoUpgrade2x = true
+getgenv().AutoUpgrade = false
+getgenv().AutoReplay = true
+getgenv().AutoJoinGame = true
 
+--get currunt cords to place unit on urself
 local x = game.Players.LocalPlayer.Character.Torso.Position.x
 local y = game.Players.LocalPlayer.Character.Torso.Position.y
 local z = game.Players.LocalPlayer.Character.Torso.Position.z
 
-
-if game.PlaceId == 6593190090 then
+--auto join in lobby
+if game.PlaceId == 6558526079 then
 
     spawn(function()
-        if getgenv().AutoSkip == true then 
+        if getgenv().AutoJoinGame == true then 
             local args = {
-                [1] = "AutoSkip"
+                [1] = {
+                    ["StageSelect"] = "Evil Pink Dungeon",
+                    ["Image"] = "rbxassetid://15289588795",
+                    ["FriendOnly"] = true,
+                    ["Difficult"] = "Nightmare"
+                }
             }
-            game:GetService("ReplicatedStorage").Remote.Setting:FireServer(unpack(args))
+            
+            game:GetService("ReplicatedStorage").Remote.CreateRoom:FireServer(unpack(args))
+            
+            wait(2)
+            
+            function clickUI(gui)
+                local GuiService = game:GetService("GuiService")
+                local VirtualInputManager = game:GetService("VirtualInputManager")
+            
+                GuiService.SelectedObject = (game:GetService("Players").LocalPlayer.PlayerGui.InRoomUi.RoomUI.QuickStart.TextButton)
+            
+                VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+                task.wait(0.1)
+                VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+            end
+            
+            clickUI(game:GetService("Players").LocalPlayer.PlayerGui.InRoomUi.RoomUI.QuickStart.TextButton)
         end
     end)
 
+end
+
+--auto place, upgrade, replay, speedup, start
+if game.PlaceId == 6593190090 then
+
+    --auto 3x(can be changed to 2x by changing x2 to x1 below)
     spawn(function()
         if getgenv().Auto3xSpeed == true then
             local args = {
@@ -31,12 +63,33 @@ if game.PlaceId == 6593190090 then
         end
     end)
 
+    --auto first skip/start game
     spawn(function()
         if getgenv().AutoFirstSkip == true then
             game:GetService("ReplicatedStorage").Remote.SkipEvent:FireServer()
         end
     end)
 
+    --auto replay
+    spawn(function()
+        while getgenv().AutoReplay == true do
+            function clickUI(gui)
+                local GuiService = game:GetService("GuiService")
+                local VirtualInputManager = game:GetService("VirtualInputManager")
+            
+                GuiService.SelectedObject = (game:GetService("Players").LocalPlayer.PlayerGui.EndUI.UI.Replay)
+            
+                VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+                task.wait(0.1)
+                VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+            end
+            
+            clickUI(game:GetService("Players").LocalPlayer.PlayerGui.EndUI.UI.Replay)
+            wait(1)
+        end
+    end)
+
+    --auto place unit of choice on your cords(can be changed if you know the x, y and z)
     spawn(function()
         if getgenv().AutoPlaceUnit == true then
             local args = {
@@ -54,6 +107,7 @@ if game.PlaceId == 6593190090 then
         end
     end)
 
+    --upgrade unit for each local args set
     spawn(function()
         if getgenv().AutoUpgrade2x == true then
             wait(15)
@@ -68,6 +122,18 @@ if game.PlaceId == 6593190090 then
                 [1] = workspace.Units.Killer
             }
             game:GetService("ReplicatedStorage").Remote.UpgradeUnit:InvokeServer(unpack(args))
+        end
+    end)
+
+    --spam upgrade of selected unit
+    spawn(function()
+        while getgenv().AutoUpgrade == true do
+            wait(1)
+            local args = {
+                [1] = workspace.Units.Killer
+            }
+            game:GetService("ReplicatedStorage").Remote.UpgradeUnit:InvokeServer(unpack(args))
+            wait(1)
         end
     end)
 
