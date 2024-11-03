af1f1d56942b34b732d7a7af3fcf36ba2c0ac9ee
legendary cookie <66409218+legendary-cookie@users.noreply.github.com>
2024-11-03 15:41:29 +0800

    Update killerawtdfarm.lua

--- killerawtdfarm.lua
+++ killerawtdfarm.lua
@@ -138,14 +138,16 @@ if game.PlaceId == 6593190090 then
         if getgenv().AutoUpgrade2x == true then
             wait(20)
             local args = {
-                [1] = workspace.Units.ReaperGunslinger
+                [1] = workspace.Units:FindFirstChild("Reaper Gunslinger")
             }
+            
             game:GetService("ReplicatedStorage").Remote.UpgradeUnit:InvokeServer(unpack(args))
 
             wait(15)
             local args = {
-                [1] = workspace.Units.ReaperGunslinger
+                [1] = workspace.Units:FindFirstChild("Reaper Gunslinger")
             }
+            
             game:GetService("ReplicatedStorage").Remote.UpgradeUnit:InvokeServer(unpack(args))
         end
     end)
